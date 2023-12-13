36a45f12ab7972f4248c2926042e96fdbbcfbd82
WojtG <wojtekgg431@gmail.com>
2023-12-13 21:14:30 +0100

    create controlled elements

--- src/FormSplitBill.js
+++ src/FormSplitBill.js
@@ -2,20 +2,52 @@ import { useState } from "react";
 import Button from "./Button";
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const [whoPaid, setWhoPaid] = useState("user");
+
+  const paidByFriend = bill ? bill - paidByUser : "";
   return (
     <form className="form-split-bill">
       <h2>Split a will with {selectedFriend.name}</h2>
       <label for="bill">Bill value</label>
-      <input type="text" id="bill" name="bill" />
+      <input
+        value={bill}
+        onChange={(e) => setBill(+e.target.value)}
+        type="text"
+        id="bill"
+        name="bill"
+      />
 
       <label for="y-expense">Your expense</label>
-      <input type="text" id="y-expense" name="y-expense" />
+      <input
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser((curr) =>
+            +e.target.value <= bill ? +e.target.value : curr
+          )
+        }
+        type="text"
+        id="y-expense"
+        name="y-expense"
+      />
 
       <label for="f-expense">{selectedFriend.name} expense</label>
-      <input disabled type="text" id="f-expense" name="f-expense" />
+      <input
+        value={paidByFriend}
+        disabled
+        type="text"
+        id="f-expense"
+        name="f-expense"
+      />
 
       <label for="personpaying">Who is paying?</label>
-      <select id="personpaying" name="personpaying">
+      <select
+        value={whoPaid}
+        onChange={(e) => setWhoPaid(e.target.value)}
+        id="personpaying"
+        name="personpaying"
+      >
         <option value="user">Me</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
